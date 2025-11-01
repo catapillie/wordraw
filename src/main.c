247c3d49bdf516e7b16a4c4e65dbcec3ec42c88d
catapillie <espritox@gmail.com>
2025-11-01 03:21:58 +0100

    add pattern-matching option

--- src/main.c
+++ src/main.c
@@ -78,13 +78,90 @@ void solve()
         solve_row(y);
 }
 
-int main(int argc, char *argv[])
+static trie_t *matching_guesses = NULL;
+static bool found_matching_guess = false;
+bool find_matching_pattern_with_answer(const char *guess, uint32_t *)
 {
-    if (argc != 2)
+    if (wordle_play(guess, answer) == current_wanted_row)
     {
-        fprintf(stderr, "USAGE:\n    ./a.out <answer>\n");
+        trie_add_word(matching_guesses, guess);
+        found_matching_guess = true;
+    }
+    return false;
+}
+
+bool log_trie(const char *word, uint32_t *)
+{
+    printf(" %s", word);
+    return false;
+}
+
+bool log_answer_matching_pattern(const char *word, uint32_t *)
+{
+    answer = word;
+    matching_guesses = trie_make();
+    found_matching_guess = false;
+    trie_iter_language(guesses, find_matching_pattern_with_answer);
+    if (found_matching_guess)
+    {
+        printf("%s ->>", word);
+        trie_iter_language(matching_guesses, log_trie);
+        printf("\n");
+    }
+    trie_destroy(matching_guesses);
+    return false;
+}
+
+int solve_pattern(const char pattern[])
+{
+    if (strlen(pattern) != 5)
+    {
+        fprintf(stderr, "ERROR: pattern must be five letters long; got '%s'.\n", pattern);
         return EXIT_FAILURE;
     }
+
+    wordle_decoded_t row;
+    for (int i = 0; i < WORDLE_WIDTH; i++)
+    {
+        switch (pattern[i])
+        {
+        case 'o':
+            row[i] = CELL_BLACK;
+            break;
+        case 'y':
+            row[i] = CELL_YELLOW;
+            break;
+        case 'g':
+            row[i] = CELL_GREEN;
+            break;
+        default:
+            fprintf(stderr, "ERROR: invalid pattern character '%c'.\n", pattern[i]);
+            fprintf(stderr, "       pattern must only contain 'o', 'y' or 'g'; got '%s'.\n", pattern);
+            return EXIT_FAILURE;
+        }
+    }
+
+    current_wanted_row = wordle_encode(row);
+    trie_iter_language(answers, log_answer_matching_pattern);
+
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[])
+{
+    answers = read_word_list("assets/answers.txt");
+    guesses = read_word_list("assets/guesses.txt");
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE:\n");
+        fprintf(stderr, "        ./a.out <answer>\n");
+        fprintf(stderr, "        ./a.out ?<pattern>\n");
+        return EXIT_FAILURE;
+    }
+
+    if (argv[1][0] == '?')
+        return solve_pattern(argv[1] + 1);
 
     answer = argv[1];
     if (strlen(answer) != 5)
@@ -93,20 +170,18 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    answers = read_word_list("assets/answers.txt");
-    guesses = read_word_list("assets/guesses.txt");
-    trie_iter_language(guesses, populate_wordle_plays);
-
     if (trie_search_word(answers, answer) == NULL)
     {
         fprintf(stderr, "ERROR: '%s' is not a valid Wordle answer.\n", answer);
         return EXIT_FAILURE;
     }
 
+    trie_iter_language(guesses, populate_wordle_plays);
     solve();
 
-    const int width = 16 * 5 * 6;
-    const int height = 16 * 5 * 6;
+    const int scale = 5;
+    const int width = 16 * scale * 6;
+    const int height = 16 * scale * 6;
     char title[50];
     sprintf(title, "Wordraw: draw on wordle [%s]", answer);
     SetTraceLogLevel(LOG_NONE);
@@ -120,7 +195,6 @@ int main(int argc, char *argv[])
     icon_x = LoadTexture("assets/img/x.png");
     float cell_width = icons[0].width;
     float cell_height = icons[0].height;
-    const float scale = 5.0;
 
     while (!WindowShouldClose())
     {
